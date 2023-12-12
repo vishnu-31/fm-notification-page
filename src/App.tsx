16c0963580ec3c546ad1c402723149f2ab3843ff
vishnu-31 <vishnuvicky3636@gmail.com>
2023-12-13 02:08:15 +0530

    added mark as read functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import { Notification } from "./Notifcation"
 
 const NotificationList =[{
@@ -21,27 +22,28 @@ const NotificationList =[{
   link: "Chess Club",
   time:"1 day ago"
 },{
-  unread:true,
+  unread:false,
   avatar:"/images/avatar-rizky-hasanuddin.webp",
   username:"Rizky Hasanuddin",
   message:"sent you a private message",
   time:"5 days ago",
   taggedMessage:"Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and I'm already having lots of fun and improving my game."
 },{
-  unread:true,
+  unread:false,
   avatar:"/images/avatar-kimberly-smith.webp",
   username:"Kimberly Smith",
   message:"commented on your picture",
-  time:"1 week ago"
+  time:"1 week ago",
+  image:{src:"/images/image-chess.webp", alt:"chess game"}
 },{
-  unread:true,
+  unread:false,
   avatar:"/images/avatar-nathan-peterson.webp",
   username:"Nathan Peterson",
   message:"reacted to your recent post",
   link: "5 end-game strategies to increase your win rate",
   time:"2 weeks ago"
 },{
-  unread:true,
+  unread:false,
   avatar:"/images/avatar-anna-kim.webp",
   username:"Anna Kim",
   message:"left the group",
@@ -52,17 +54,27 @@ const NotificationList =[{
 
 
 function App() {
+  const [isRead, setIsRead] = useState(false);
+
+  const toggleRead = () =>{
+    if(!isRead){
+      for(const i of NotificationList){
+        i.unread=false
+      }
+      setIsRead(true);
+  }
+  }
 
   return (
     <main className='bg-white rounded w-screen min-h-screen p-5 justify-center lg:w-[40em] lg:min-h-fit'>
       <div className="flex justify-between">
-        <div className='flex'><h1 className='font-bold text-xl'>Notfications</h1><span className='mx-3 px-3 text-white rounded-lg bg-blue-900'>3</span></div>
-        <button className='text-gray-500 hover:text-blue-400' type="button">Mark all as read</button>
+        <div className='flex'><h1 className='font-bold text-xl'>Notfications</h1>{!isRead && <span className='mx-3 px-3 text-white rounded-lg bg-blue-900'>3</span>}</div>
+        <button className='text-gray-500 hover:text-blue-400' type="button" onClick={toggleRead}>Mark all as read</button>
       </div>
       <div className="flex flex-col w-full my-3">
-        {NotificationList.map(item,idx) =>(
-          <Notification key={idx} {..item}/>
-        )}
+        {NotificationList.map((item,idx) =>(
+          <Notification key={idx} {...item}/>
+        ))}
         
       </div>
     </main>
--- src/Notifcation.tsx
+++ src/Notifcation.tsx
@@ -17,21 +17,21 @@ export const Notification: FC<NotifcationParams> = (props:NotifcationParams) => 
 
     return (
         <div className={`flex justify-start rounded p-3 ${props.unread? "bg-blue-100":""} min-w-full`}>
-            <img className="w-10 h-10 rounden-full mx-2" src="/images/avatar-angela-gray.webp" alt="Angela Gray" />
+            <img className="w-10 h-10 rounden-full mx-2" src={props.avatar} alt={props.username} />
             <div className="mx-2 flex flex-col w-full">
                 <div className="flex justify-between mx-0 min-w-full">
                     <div className="flex flex-col">
                         <div className="text-sm flex">
                             <span className="inline">
-                                <a className=" mx-1 font-bold">{props.username}</a>
+                                <a className=" mx-1 font-bold cursor-pointer hover:text-blue-500">{props.username}</a>
                                 <a className="mx-1">{props.message} </a>
-                                {!!props.link && <a className="font-semibold mx-1 cursor-pointer">{props.link}</a>}
+                                {!!props.link && <a className="font-semibold mx-1 cursor-pointer hover:text-blue-500">{props.link}</a>}
                             </span>
-                            {props.unread && <FaCircle color='red' className="w-2 h-2"/>}
+                            {props.unread && <FaCircle color='red' className="w-[8px]  h-[8px] mt-2"/>}
                         </div>
                         <div className="text-sm my-1 text-gray-500">{props.time}</div>
                     </div>
-                    {!!props.image && <img className="h-10 w-10 ml-4" src={props.image.src} alt={props.image.alt} />}
+                    {!!props.image && <img className="h-10 w-10 ml-4 cursor-pointer" src={props.image.src} alt={props.image.alt} />}
                 </div>
                 
                 {!!props.taggedMessage && <div className="flex border-[1px] rounded p-3 text-sm border-gray-300 transition-color ease-in-out duration-300 cursor-pointer hover:bg-blue-300">
